a2de3485aa97b0834dc72a8f1650196f67d824a0
Arie A <dropbox@mountrash.com>
2024-01-04 21:39:27 +0700

    API URL

--- app/constants.ts
+++ app/constants.ts
@@ -1,2 +1,3 @@
-export const API_URL = "http://localhost:1337";
+// export const API_URL = "http://localhost:1337";
+export const API_URL = "https://api.misteryoscourse.com";
 export const TOKEN_KEY = "strapi-jwt-token";
